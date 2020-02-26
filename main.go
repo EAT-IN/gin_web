8a6ee11ee92e4f6c35bc43ee92dd0d2f7cad16ee
xmzhang.abcft <25337942@qq.com>
2020-02-26 12:54:17 +0800

    [add] 完成任务清单的api定义

--- main.go
+++ main.go
@@ -1,29 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/cookie", func(c *gin.Context) {
-		// 先获取cookie值，如果没有就设定一个cookie
-		name, err := c.Cookie("name")
-		if err != nil {
-			name = "not set"
-			// name:cookie的名字
-			// value：cookie的值
-			// maxage：cookie的有效时间， 单位为秒
-			// path是指 cookie所在的目录
-			// domain 域名
-			// secure 是否只能通过https 访问
-			// httpone 是否允许别人通过js获取自己的cookie
-			c.SetCookie("name", "xmzhang", 3600, "/", "localhost", false, true)
-		}
-		fmt.Println("cookie的值为：", name)
+	r.Static("/static", "static")
+	r.StaticFile("favicon.ico", "template/favicon.ico")
+
+	r.LoadHTMLGlob("template/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	r.Run()
+	v1 := r.Group("/v1")
+	{
+		// 增加事项
+		v1.POST("/todo", func(c *gin.Context) {
+
+		})
+
+		// 查看事项
+		v1.GET("/todo", func(c *gin.Context) {
+
+		})
+		v1.GET("/todo/:id", func(c *gin.Context) {
+
+		})
+
+		// 更改事项
+		v1.PUT("/todo/:id", func(c *gin.Context) {
+
+		})
+
+		// 删除事项
+		v1.DELETE("/todo/:id", func(c *gin.Context) {
+
+		})
+
+	}
+
+	r.Run(":5000")
 }
